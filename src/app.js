c1aea3b1572fe4c7237aa0ee4549d57b7771e4a9
Mehmet AYBEY <mehmetaybey@aof.anadolu.edu.tr>
2021-10-19 00:02:15 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("Merhaba Mehmet Aybey")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+// euroDun=11
+console.log(euroDun)
+let konutKredileri = ["Özel konut kredisi","Konut Kredisi", "Emlak Konut Kredisi", "Kamu KonutKredileri"]
+
+console.log("<ul>")
+
+for (let i = 0; i < konutKredileri.length ;i++) {
+    console.log("<li>"+konutKredileri[i] +"</li>")
+    
+}
+console.log("</ul>")
+
+//console.log(konutKredileri)
